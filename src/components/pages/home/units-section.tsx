702dd80fd6c5374ac899ded4920c51c4006a6d07
OsamaHIma <osamahima018@gmail.com>
2026-02-06 17:11:58 +0200

    push

--- src/components/pages/home/units-section.tsx
+++ src/components/pages/home/units-section.tsx
@@ -1,5 +1,5 @@
 import { useTranslations } from "next-intl";
-import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const UnitsSection = () => {
   const t = useTranslations();
@@ -7,10 +7,22 @@ const UnitsSection = () => {
     <div className="h-[90svh] bg-main-50 relative">
       <img src="/section-bg.svg" alt="" className="absolute top-0 start-0" />
       <div className="container py-[17svh]">
-        <div className="flex items-center gap-3">
-          <img src="/section-logo.svg" alt="" />
-          <h2 className="section-title">{t("Units Schedule")}</h2>
-        </div>
+        <Tabs defaultValue="account">
+          <div className="flex items-center justify-between gap-5 flex-wrap">
+            <div className="flex items-center gap-3">
+              <img src="/section-logo.svg" alt="" />
+              <h2 className="section-title">{t("Units Schedule")}</h2>
+            </div>
+            <TabsList>
+              <TabsTrigger value="account">Account</TabsTrigger>
+              <TabsTrigger value="password">Password</TabsTrigger>
+            </TabsList>
+          </div>
+          <TabsContent value="account">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="password">Change your password here.</TabsContent>
+        </Tabs>
       </div>
     </div>
   );
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,22 +1,23 @@
-import * as React from 'react'
-import * as TabsPrimitive from '@radix-ui/react-tabs'
+import * as React from "react";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
 
-import { ScrollArea, ScrollBar } from './scroll-area'
-import { cn } from '@/lib/utils'
+import { ScrollArea, ScrollBar } from "./scroll-area";
+import { cn } from "@/lib/utils";
+import { useLocale } from "next-intl";
 
 function Tabs({
   className,
   ...props
 }: React.ComponentProps<typeof TabsPrimitive.Root>) {
+  const locale = useLocale();
   return (
     <TabsPrimitive.Root
       data-slot="tabs"
-      className={cn('flex flex-col gap-2', className)}
-      // @ts-ignore
-      dir="auto"
+      className={cn("flex flex-col gap-2", className)}
+      dir={locale === "ar" ? "rtl" : "ltr"}
       {...props}
     />
-  )
+  );
 }
 
 function TabsList({
@@ -28,36 +29,36 @@ function TabsList({
       <TabsPrimitive.List
         data-slot="tabs-list"
         className={cn(
-          'bg-card text-muted-foreground inline-flex h-12 w-fit items-center justify-center rounded-full p-[3px]',
+          "bg-transparent text-muted-foreground inline-flex h-12 w-fit items-center justify-center rounded-full p-[3px]",
           className,
         )}
         {...props}
       />
       <ScrollBar orientation="horizontal" />
     </ScrollArea>
-  )
+  );
 }
 
 function TabsTrigger({
   className,
-  variant = 'default',
+  variant = "default",
   ...props
 }: React.ComponentProps<typeof TabsPrimitive.Trigger> & {
-  variant?: 'default' | 'secondary'
+  variant?: "default" | "secondary";
 }) {
   return (
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "ltr:font-poppins rtl:font-ping data-[state=active]:bg-primary data-[state=active]:text-white dark:data-[state=active]:text-white focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-full border border-transparent px-3 py-2 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "bg-card data-[state=active]:bg-primary data-[state=active]:text-white dark:data-[state=active]:text-white focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-full border border-transparent px-3 py-2 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className,
-        variant === 'secondary'
-          ? 'rounded-xl data-[state=active]:bg-card data-[state=active]:text-foreground flex items-center gap-3'
-          : '',
+        variant === "secondary"
+          ? "rounded-xl data-[state=active]:bg-card data-[state=active]:text-foreground flex items-center gap-3"
+          : "",
       )}
       {...props}
     />
-  )
+  );
 }
 
 function TabsContent({
@@ -67,10 +68,10 @@ function TabsContent({
   return (
     <TabsPrimitive.Content
       data-slot="tabs-content"
-      className={cn('flex-1 outline-none', className)}
+      className={cn("flex-1 outline-none", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export { Tabs, TabsList, TabsTrigger, TabsContent };
