ec01310bf6cff0bb6b4628db6b2ac2b5c950ab75
OsamaHIma <osamahima018@gmail.com>
2026-02-16 20:20:29 +0200

    push

--- src/components/pages/projects/index.tsx
+++ src/components/pages/projects/index.tsx
@@ -51,7 +51,7 @@ const ProjectsList = () => {
       </div>
 
       <div className="container py-20 space-y-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 gap-y-12">
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 gap-y-12">
           {data?.data?.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
